3ffb72755abef6a2dba9f6bcc4775744152df30b
Suraj <sv961988@gmail.com>
2024-04-11 12:01:48 -0700

    hero base done > without image

--- src/assets/components/Hero.tsx
+++ src/assets/components/Hero.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "../styles/Hero.css";
+import hero_bg from "/images/bg.jpg";
 
 const Hero = () => {
-  return <div>Home </div>;
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        <h1>Welcome to Our Website</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+          convallis libero vel orci fermentum, vel ultrices lorem cursus.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
