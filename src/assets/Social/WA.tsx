e48c2f7acf6fe2cf4e7ecbf2fa12460fa59b6cb4
Suraj <sv961988@gmail.com>
2024-05-01 00:16:05 +0530

    .

--- src/assets/Social/WA.tsx
+++ src/assets/Social/WA.tsx
@@ -1,10 +1,21 @@
 import "../Social/WA.css";
 import { FaWhatsapp } from "react-icons/fa6";
 const WA = () => {
+  const openWhatsAppChat = (phoneNumber: number, message = "") => {
+    const encodedMessage = encodeURIComponent(message);
+    const url = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+    window.open(url, "_blank"); // Open in a new tab/window
+  };
+
   return (
     <>
       <div className="floating-whatsapp-btn">
-        <div className="whatsapp-btn">
+        <div
+          className="whatsapp-btn"
+          onClick={() =>
+            openWhatsAppChat(1234567890, "Hi there, I have a question...")
+          }
+        >
           <FaWhatsapp />
         </div>
       </div>
