14483d1a30efcf070a826d06722fb90ca3657a0b
Suraj <sv961988@gmail.com>
2024-04-30 23:30:50 +0530

    whatsapp icon added

---
+++ src/assets/Social/WA.tsx
@@ -0,0 +1,15 @@
+import "../Social/WA.css";
+import { FaWhatsapp } from "react-icons/fa6";
+const WA = () => {
+  return (
+    <>
+      <div className="floating-whatsapp-btn">
+        <div className="whatsapp-btn">
+          <FaWhatsapp />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default WA;
--- src/assets/components/Foot.tsx
+++ src/assets/components/Foot.tsx
@@ -1,10 +1,14 @@
 import "../styles/Foot.css";
 import ScrollToTop from "../components/ScrollToTop";
+import WhatsApp from "../Social/WhatsApp";
+import WA from "../Social/WA";
 const Foot = () => {
   return (
     <footer className="footer">
       <p>&copy; 2024 FrameSquare. All Rights Reserved.</p>
       <ScrollToTop />
+      <WhatsApp />
+      <WA />
     </footer>
   );
 };
