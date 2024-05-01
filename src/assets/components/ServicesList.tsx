cac55c2a5fe2aecdd0b33f4bf2dba76c19d4cd55
Suraj <sv961988@gmail.com>
2024-05-01 16:28:49 +0530

    services list done

--- src/assets/components/ServicesList.tsx
+++ src/assets/components/ServicesList.tsx
@@ -1,5 +1,26 @@
+import "../styles/ServicesList.css";
+import Card from "../suppoters/Card";
+import servicesData from "../suppoters/services.json";
+
 const ServicesList = () => {
-  return <div>services</div>;
+  return (
+    <>
+      <div className="services-list">
+        <h2 className="title">Services</h2>
+        <div className="services-list-container">
+          {servicesData.map((service) => (
+            <div className="service-list" key={service.id}>
+              <Card
+                title={service.title}
+                description={service.description}
+                imageUrl={service.imageUrl}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ServicesList;
--- src/assets/suppoters/Card.tsx
+++ src/assets/suppoters/Card.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 
-const Card = ({ imageUrl, title, description }) => {
+interface CardProps {
+  title: string;
+  description: string;
+  imageUrl: string;
+  className?: string; // Optional custom class name
+}
+
+const Card: React.FC<CardProps> = ({
+  imageUrl,
+  title,
+  description,
+  className = "",
+}) => {
   return (
-    <div className="card">
-      <img src={imageUrl} alt={title} />
+    <div className={`card ${className}`}>
+      {imageUrl && <img src={imageUrl} alt={title} />}
       <h3>{title}</h3>
       <p>{description}</p>
     </div>
